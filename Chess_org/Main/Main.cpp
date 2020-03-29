9900834c376b27324ea8fd45239d7b740e5d3b4b
Jurymax99 <jurymax99@gmail.com>
2020-03-29 04:55:45 +0200

    Add files via upload

--- Chess_org/Main/Main.cpp
+++ Chess_org/Main/Main.cpp
@@ -5,3 +5,12 @@ int main() {
 	Application app;
 	app.start();
 }
+
+/*
+minimal
+F 11c  F 12c  F 21l  F 22l F 20l F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 1  F 4  F 5  F 19l  F 7  F 8  F 9  F 10  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 19l  F 20l  F 21l  F 20l  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 1  F 4  F 5  F 19l  F 7  F 8  F 9  F 10  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 19l  F 20l  F 21l  F 20l  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 1  F 4  F 5  F 19l  F 7  F 8  F 9  F 10  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 19l  F 20l  F 21l  F 20l  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 1  F 4  F 5  F 19l  F 7  F 8  F 9  F 10  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 19l  F 20l  F 21l  F 20l  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 1  F 4  F 5  F 19l  F 7  F 8  F 9  F 10  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 19l  F 20l  F 21l  F 20l  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 1  F 4  F 5  F 19l  F 7  F 8  F 9  F 10  F 11c  F 12c  F 21l  F 14c  F 15c  F 16c  F 17c  F 18c  F 3  F 19l  F 20l  F 21l  F 20l  
+*/
+/*
+test
+F 11c  F 12c  F 21l  F 22l F 20l F 14c
+*/
